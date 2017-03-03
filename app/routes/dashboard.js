e7f58a2900fc945210b1b8f9e3734799e1c6de39
Andres Vizcaino <andruvizcaino1989@gmail.com>
2017-03-03 17:42:26 -0500

    Going straight to the overview

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -3,6 +3,14 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   session: Ember.inject.service(),
+  beforeModel(transition) {
+    // trigger AuthenticatedRouteMixin's beforeModel
+    this._super(...arguments);
+
+    if (transition.targetName === 'dashboard.index') {
+      this.transitionTo('dashboard.overview');
+    }
+  },
   actions: {
     logout() {
       this.get('session').invalidate();
